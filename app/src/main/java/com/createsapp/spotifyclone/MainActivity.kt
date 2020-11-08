a32f821db5bbca44e8737f1415e089facb0f28e2
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-11-08 20:04:42 +0530

    Dagger-Hilt Setup

--- app/src/main/java/com/createsapp/spotifyclone/MainActivity.kt
+++ app/src/main/java/com/createsapp/spotifyclone/MainActivity.kt
@@ -2,12 +2,22 @@ package com.createsapp.spotifyclone
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.RequestManager
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 //Glam database from mahesh223prasad
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var glide: RequestManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
